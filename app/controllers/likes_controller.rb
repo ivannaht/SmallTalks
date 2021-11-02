1f48b0706a82b73d4bd200a26c3110430e2fb75f
ivannaht <ivahontaryk32@gmail.com>
2021-11-02 17:00:55 +0200

    Added likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,42 @@
 class LikesController < ApplicationController
     before_action :find_talk
-  def create
-    @talk.likes.create(user_id: current_user.id)
+    before_action :find_like, only: [:destroy]
+  
+    def already_liked?
+        Like.where(user_id: current_user.id, talk_id:
+        params[:talk_id]).exists?
+    end  
+  
+    def create
+        if already_liked?
+        flash[:notice] = "You can't like more than once"
+      else
+        @talk.likes.create(user_id: current_user.id)
+      end
+      redirect_to talk_path(@talk)
+  end
+
+  def destroy
+    if !(already_liked?)
+      flash[:notice] = "Cannot unlike"
+    else
+      @like.destroy
+    end
     redirect_to talk_path(@talk)
   end
+
   private
   def find_talk
     @talk = Talk.find(params[:talk_id])
   end
+
+
+  def find_like
+    @like = @talk.likes.find(params[:id])
+  end
+
+  
+
+
+ 
 end
