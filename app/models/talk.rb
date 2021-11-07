412a059eee2f9a8eca77e9bc55f1ed28a78a140d
ivannaht <ivahontaryk32@gmail.com>
2021-11-07 13:30:26 +0200

    Updated FactoryBot for talks

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -3,6 +3,7 @@ class Talk < ApplicationRecord
   validates :title, :text, :photo, :theme, presence: true 
   
   validates :title, uniqueness: true, format: {with: /[A-Za-z0-9_]{3,30}/}
+  
   validates :theme, format: {with: /[A-Za-z0-9_]{3,15}/}
 
  
---
+++ spec/factories/talks.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+    factory :talk do
+       title { "Test title for Pets" }    
+        text { "Test text for Pets" }
+        photo { Rack::Test::UploadedFile.new('spec/support/test_image.jpg', 'image/jpg') }
+        theme { "Pets" }              
+        user_id {15}
+    end
+
+    factory :random_talk, class: Talk do
+        title {Faker::Lorem.sentence(word_count: 3)}
+        text {Faker::Lorem.paragraph(sentence_count: 3)}
+        photo {Rack::Test::UploadedFile.new('spec/support/test_image.jpg', 'image/jpg') }
+        theme {Faker::Hobby.activity}
+        user_id {15}
+    end
+end
